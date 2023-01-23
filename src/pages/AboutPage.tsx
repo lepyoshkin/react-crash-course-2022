6a255718e5a84200c3029dbeba27ce33bea1a481
lepyoshkin <lepyoshkin.artem@gmail.com>
2023-01-23 08:55:29 +0200

    complete course version

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id hic deserunt
+      error maiores ratione reprehenderit, unde nesciunt fuga magni laborum
+      tempora earum perferendis exercitationem beatae inventore eius sed est!
+      Accusantium!
+    </div>
+  );
+}
